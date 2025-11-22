906ff3d6352c3c5dbd73f886dccea1d48c4a1614
jcvl-udg <jcarlos.vargas@udg.mx>
2025-11-21 21:27:20 -0600

    1st
    setup general, se configura Auth0 sin lograr aun el Login

---
+++ TODO.md
@@ -0,0 +1,11 @@
+## Setup List
+
+- [x] Laravel Dockerized
+- [x] Auth0 Register - Laravel/Library installed
+
+
+### Por hacer
+
+- [] Configurar el Auth0 
+- [] Configurar el R3F
+- [] Login y guardar posicion en el mundo antes del Logout
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
-
+use Auth0\Laravel\Facade\Auth0;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Public Welcome Route
+|--------------------------------------------------------------------------
+*/
 Route::get('/', function () {
+    // If user is logged in, show their name/email directly
+    if (auth()->check()) {
+        $user = auth()->user();
+        return response("Hello {$user->name}! Your email is {$user->email}. <a href='/logout'>Logout</a>");
+    }
+
+    // If not logged in, show the welcome view with a login link
     return view('welcome');
 });
+
+/*
+|--------------------------------------------------------------------------
+| Protected Route (The "Private" Page)
+|--------------------------------------------------------------------------
+*/
+Route::get('/private', function () {
+    // Only accessible if logged in
+    return "This is a private secret! Only for " . auth()->user()->name;
+})->middleware('auth');
